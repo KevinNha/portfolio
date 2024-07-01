7cface298c9e41afff79108ab260c065031e799f
Hyunjin Nha <hyunjinnha@gmail.com>
2024-06-30 23:13:32 -0700

    Fix svg height

--- src/lib/assets/Darkmode.svelte
+++ src/lib/assets/Darkmode.svelte
@@ -1,10 +1,4 @@
-<svg
-	width="800px"
-	height="800px"
-	viewBox="0 0 24 24"
-	fill="none"
-	xmlns="http://www.w3.org/2000/svg"
->
+<svg width="10px" height="10px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 	<path
 		fill-rule="evenodd"
 		clip-rule="evenodd"
